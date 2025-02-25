1a064d3b14177aeb9ff228ee2ddc24743d6e2176
Diogobq7 <diogo.queiroz@germinare.org.br>
2025-02-25 08:52:36 -0300

    Adicionando Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <NavbarComponent />
     </>
   )
 }
