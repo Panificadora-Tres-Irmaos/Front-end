ee7b39c94d051e8c8245f5820c503f4c6022553d
daniell0154 <danieldejesusferreira01@gmail.com>
2025-03-08 18:26:18 -0300

    Melhorias na NavBar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,14 +9,16 @@ function NavbarComponent() {
     <Navbar expand="lg" className={styles.customNavbar}>
       <Container className={styles.container}>
         <Navbar.Brand href="#home" className={styles.titleNav}>
-          <img src='src\assets\images\logo_tres_contrario.png'></img>
+          <img src='src\assets\logo\logo_tres_contrario.png'></img>
           <p>Panificadora Três Irmãos</p>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" id={styles.botaoMenu}/>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav>
+          <Nav id={styles.contLinks}>
             <Nav.Link href="#produtos" className={styles.links}>Produtos</Nav.Link>
-            <Nav.Link href="#perfil" className={styles.links}>Perfil</Nav.Link>
+            <Nav.Link href="#perfil" className={styles.links}>
+              <img src="src\assets\images\carrinho-de-compras.png" alt="" />
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
