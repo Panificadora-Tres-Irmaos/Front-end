feaa25fcaf1dcb34dd6a0c38d31cd9db694ac3ff
daniell0154 <danieldejesusferreira01@gmail.com>
2025-03-10 09:50:23 -0300

    Atribuindo melhorias na estrutura de pastas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComponent from './components/Navbar'
+import NavbarComponent from './components/navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
